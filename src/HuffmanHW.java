47ba8ff55ab134a78d418180e8cdc852b4bd0c36
yigitkarakurt <yigitkarakurt35@gmail.com>
2022-12-23 20:39:23 +0300

    huffman

--- src/HuffmanHW.java
+++ src/HuffmanHW.java
@@ -1,17 +1,65 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Objects;
 import java.util.Scanner;
 
-public class HuffmanHW {
+public class HuffmanHW<T extends Comparable> {
 
-    public static void huffmanOdev() throws FileNotFoundException {
+    private TNode<T> root;
+
+    public void findFreq(String letter){
+
+
 
     }
 
-    public static void main(String[] args){
+    public String encode(String symbol) {
+
+        if (Objects.equals(symbol, "")){
+            return "";
+        }
+
+        String encodedSymbol = "";
+
+        for (int i = 0; i < symbol.length(); i++) {
+            String character = String.valueOf(symbol.charAt(i));
+            TNode<T> iterator = root;
+
+            while(iterator != null) {
+                if (iterator.symbol.compareTo(character) == 0){
+                    break;
+                }
+
+                if (iterator.left != null && iterator.left.symbol.contains(character)){
+                    encodedSymbol += "0";
+                    iterator = iterator.left;
+                }else{
+                    encodedSymbol += ("1");
+                    iterator = iterator.right;
+                }
+            }
+        }
+
+        return encodedSymbol;
+    }
+
+    public String decode() {
+        String decodedSymbol = "";
+
+
+
+        return decodedSymbol;
+    }
+
+    public static void main (String[]args){
+
+        HuffmanHW<String> huffman = new HuffmanHW<>();
+
+        System.out.println(huffman.encode("yigit"));
 
 
     }
-
 
 }
+
+
--- src/TNode.java
+++ src/TNode.java
@@ -1,10 +1,21 @@
 public class TNode<T extends Comparable> implements Comparable {
-    T value;
-    TNode<T> left;
-    TNode<T> right;
+    public T value;
+    public TNode<T> left;
+    public TNode<T> right;
+
+    public String symbol;
+    public double freq;
     public TNode(T val){
         value=val;
         left=right=null;
+    }
+
+    public TNode(String symbol, double freq){
+        this.symbol = symbol;
+        this.freq = freq;
+        left=right=null;
+
+
     }
     public String toString(){
         return String.valueOf(value);
@@ -13,13 +24,10 @@ public class TNode<T extends Comparable> implements Comparable {
 
     @Override
     public int compareTo(Object o) {
-        int a = (int)this.value;
-        int b = (int) o;
+        String b = (String) o;
 
-        if (a == b){
+        if (((String) o).equalsIgnoreCase(b)){
             return 0;
-        }else if (a > b){
-            return 1;
         }else {
             return -1;
         }
